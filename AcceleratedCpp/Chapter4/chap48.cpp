97889633c4eb5d88563eaf8548c80099a65f1627
dsocaciu <dan.socaciu@gmail.com>
2017-02-23 21:29:11 -0500

    Finished Chapter 4

---
+++ AcceleratedCpp/Chapter4/chap48.cpp
@@ -0,0 +1,8 @@
+//Accelerated C++
+//Chapter 4 Organizing programs and data
+//Question 4-8 Student info header file
+
+// If the following code is legal, what can we infer about the return type of f?
+	double d = f()[n];
+
+//must be of type double since d is a double
